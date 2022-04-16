23a912411d9184b8c7e34b5548274504518915d0
Dongdong Gao <gaodongdong2012@gmail.com>
2022-04-16 15:26:59 +0800

    update scripts

--- build_xcframework.sh
+++ build_xcframework.sh
@@ -4,5 +4,5 @@ rm -rf build/
 mkdir build 
 cd build 
 
-cmake .. -DPLATFORM=OS -DCMAKE_BUILD_TYPE=Release --toolchain=../ios.toolchain.cmake -DDEPLOYMENT_TARGET=9.0
-cmake --build .
+cmake .. -DLIBRARY_TYPE=SHARED -DPLATFORM=OS -DCMAKE_BUILD_TYPE=Release --toolchain=../ios.toolchain.cmake -DDEPLOYMENT_TARGET=9.0
+cmake --build . --config release
